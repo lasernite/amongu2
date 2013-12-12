29e8ce09c386b7029633f0ac2f09fdf85a3de766
Siddharth Trehan <trehansiddharth@gmail.com>
2013-12-12 18:09:06 -0500

    Prototype

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var app = express();
 
+var port = parseInt(process.env.PORT, 10) || 8080;
+
+app.use('/public', express.static(__dirname + '/public'));
+
 app.get("/", function (request, response) {
-	response.end("hello world!");
+	response.sendfile("index.html");
 });
 
-app.listen(8080);
+app.use(function (request, response, next) {
+    if (request.path.substring(0, 3) == "/u/")
+    {
+        response.end("user page")
+    }
+    else if (request.path.substring(0, 3) == "/c/")
+    {
+        response.end("comment page")
+    }
+})
+
+app.listen(port);
+
+console.log("Now listening on port " + port);
